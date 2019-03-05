95831b26eef13c2338697b9d4ea9579330591a8b
Hubert Dumas <hubert.dumas@frontiersoftware.com.au>
2019-03-05 15:54:54 +1100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { OidcSecurityService } from "angular-auth-oidc-client";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'ngxclient';
+export class AppComponent implements OnInit {
+  title = "client";
+
+  constructor(private oidc: OidcSecurityService) {
+  }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.oidc.authorize();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, APP_INITIALIZER } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {
+  AuthModule,
+  OidcSecurityService,
+  OpenIDImplicitFlowConfiguration,
+  OidcConfigService,
+  AuthWellKnownEndpoints
+} from "angular-auth-oidc-client";
+
+
+export function initializeApp(oidcConfigService: OidcConfigService) {
+  return () => oidcConfigService.load_using_stsServer("http://localhost:5000");
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +23,82 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    OidcConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      deps: [OidcConfigService],
+      multi: true
+    },
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(
+    private oidcConfigService: OidcConfigService,
+    private oidcSecurityService: OidcSecurityService
+  ) {
+
+    this.oidcConfigService.onConfigurationLoaded.subscribe(() => {
+
+      const originUrl = "http://localhost:4200";
+      const authUrl = "http://localhost:5000";
+
+      this.oidcConfigService.clientConfiguration = {
+        stsServer: authUrl,
+        client_id: "resClient",
+        redirect_url: originUrl + "/signin-oidc" ,
+        response_type: "id_token token",
+        scope: "openid profile api1",
+        post_logout_redirect_uri: originUrl + "/signout-callback-oidc",
+        start_checksession: false,
+        silent_renew: false,
+        forbidden_route: authUrl + "forbidden",
+        unauthorized_route: authUrl + "unauthorized",
+        log_console_warning_active: true,
+        log_console_debug_active: false,
+        max_id_token_iat_offset_allowed_in_seconds: 15
+      };
+      const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
+      openIDImplicitFlowConfiguration.stsServer = authUrl;
+      openIDImplicitFlowConfiguration.client_id = "spa";
+      openIDImplicitFlowConfiguration.redirect_url = originUrl + "/signin-oidc";
+      openIDImplicitFlowConfiguration.post_logout_redirect_uri = originUrl + "/signout-callback-oidc";
+      openIDImplicitFlowConfiguration.response_type = "id_token token";
+      openIDImplicitFlowConfiguration.scope = "openid profile api1";
+      openIDImplicitFlowConfiguration.forbidden_route = "/forbidden";
+      openIDImplicitFlowConfiguration.unauthorized_route = "/unauthorized";
+      openIDImplicitFlowConfiguration.auto_userinfo = true;
+      openIDImplicitFlowConfiguration.log_console_warning_active = true;
+      openIDImplicitFlowConfiguration.log_console_debug_active = true;
+      openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 10;
+
+      const authWellKnownEndpoints = new AuthWellKnownEndpoints();
+      // authWellKnownEndpoints.issuer = authUrl;
+
+      // authWellKnownEndpoints.jwks_uri = authUrl + "/.well-known/openid-configuration/jwks";
+      // authWellKnownEndpoints.authorization_endpoint = authUrl + "/connect/authorize";
+      // authWellKnownEndpoints.token_endpoint = authUrl + "/connect/token";
+      // authWellKnownEndpoints.userinfo_endpoint = authUrl + "/connect/userinfo";
+      // authWellKnownEndpoints.end_session_endpoint = authUrl + "/connect/endsession";
+      // authWellKnownEndpoints.check_session_iframe = authUrl + "/connect/checksession";
+      // authWellKnownEndpoints.revocation_endpoint = authUrl + "/connect/revocation";
+      // authWellKnownEndpoints.introspection_endpoint = authUrl + "/connect/introspect";
+      // authWellKnownEndpoints.introspection_endpoint = authUrl + "/connect/introspect";
+
+      authWellKnownEndpoints.setWellKnownEndpoints(this.oidcConfigService.wellKnownEndpoints);
+      this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration, authWellKnownEndpoints);
+
+    });
+
+  }
+
+}
+
+
